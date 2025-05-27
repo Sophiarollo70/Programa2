e9200cc8ac132378b56e01a27db72d978f12acd9
Sophiarollo70 <sophiarollo70@gmail.com>
2025-05-27 16:45:09 -0300

    primeiro commit

---
+++ raizessegundograu.py
@@ -0,0 +1,16 @@
+from math import sqrt
+A= float(input("Digite o valor de A: "))
+B= float(input("Digite o valor de B: "))
+C= float(input("Digite o valor de C: "))
+
+if A == 0:
+    print("Noo pode divisao por 0")
+else:
+    delta= (B ** 2) - (4 * A * C)
+    if delta < 0:
+        print("delta invalido - negativo")
+    else:
+        x1= (-B + sqrt(delta)) / (2 * A)
+        x2= (-B - sqrt(delta)) / (2 * A)
+        print("Raiz 1:", x1)
+        print("Raiz 2:", x2)
